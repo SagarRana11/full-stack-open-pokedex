1daefead143d095c5a20632ecfbb12d61fbf5efa
sagarrana <ranasagar974@gmail.com>
2024-10-08 22:28:02 +0530

    Fix linting errors: quotes, console logs, indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,11 +33,11 @@ module.exports = {
       'single'
     ],
     // semi: 'off',
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
+    // eqeqeq: 'error',
+    // 'no-trailing-spaces': 'error',
+    // 'object-curly-spacing': [
+    //   'error', 'always'
+    // ],
     'arrow-spacing': [
       'error', { before: true, after: true }
     ],
