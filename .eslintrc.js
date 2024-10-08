529c2a18b2e2ab991a7d1aac62976ac007be2392
sagarrana <ranasagar974@gmail.com>
2024-10-08 22:02:01 +0530

    pipe

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,10 +32,7 @@ module.exports = {
       'error',
       'single'
     ],
-    semi: [
-      'error',
-      'never'
-    ],
+    semi: 'off',
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': [
